dcb782746dc2ad6f1ceaea441eea1a6fcc60c59e
balaravanan81726 <balaravanan81726@gmail.com>
2021-10-16 18:07:08 +0530

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header/Header'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import CrouselView from './components/Crousel/Crousel';
+import DealCardViewPort from './components/DealCardViewPort/DealCardViewPort';
+import BrandInFocusViewPort from './components/BrandInFocusViewPort/BrandInFocusViewPort'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+library.add(fas)
+
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App container-fluid">
+       <Header/>
+       
+       <CrouselView/>
+        <DealCardViewPort/>
+
+        <h2>BRANDS IN FOCUS</h2>
+        <p>Show some brand love</p>
+        
+
+       <BrandInFocusViewPort/>
+       
+       
+    
+      </div>
+    );
+  }
 }
 
 export default App;
